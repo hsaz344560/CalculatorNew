391f9c8e74585187a2fef44c5cd29ff9b9710308
hsaz344560 <925107588@qq.com>
2020-02-17 20:42:19 +0800

    测试修改

--- app/src/main/java/com/example/calculatornew/Data.java
+++ app/src/main/java/com/example/calculatornew/Data.java
@@ -1,4 +1,15 @@
 package com.example.calculatornew;
-
+//测试修改
+/**
+ * 保存字符串的类
+ */
 public class Data {
+    //保存字符串
+    String str;
+    //f为真时，当前对象的str为数字，否则为操作符
+    boolean f;
+    Data(){
+        this.f=false;
+        str="";
+    }
 }
